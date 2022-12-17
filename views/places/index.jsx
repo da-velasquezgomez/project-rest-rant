383df9bf3520ad774456db7d7108a13c7801c5b8
Diego Velasquez <100886672+da-velasquezgomez@users.noreply.github.com>
2022-12-16 19:18:19 -0700

    mongodb

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,37 +1,35 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react")
+const Def = require("../default")
 
 function index (data) {
-  console.log(data)
-  let placesFormatted = data.places.map((place, index) => {
+    let placesFormatted = data.places.map((place, index) => {
+      return (
+        <div className="col-sm-6" key="{place.name}">
+          <h2>
+            <a href={`/places/${index}`}>
+              {place.name}
+            </a>
+          </h2>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name}/>
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
+        </div>
+      )
+    })
     return (
-      <div className="col-sm-6">
-        <h2>
-          <a href={`/places/${index}`}>
-            {place.name}
-          </a>
-        </h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
-        <img src={place.pic} alt={place.name} width="400" height="400"/>
-        <p className="text-center">
-          Located in {place.city}, {place.state}
-        </p>
-      </div>
+        <Def>
+            <main>
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                  {placesFormatted}
+                </div>  
+            </main>
+        </Def>
     )
-  })
-    return (
-      <Def>
-          <main>
-              <h1>PLACES INDEX PAGE</h1>
-              <div className='row'>
-                {placesFormatted}
-              </div>
-              
-          </main>
-      </Def>
-  )
   }
 
 module.exports = index
